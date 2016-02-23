e2d4059a4a77bd80225d9a06393926cfab61aff5
Matt Richardson <matt@redgumtech.com.au>
2016-02-23 21:47:49 +0000

    Adding tests for DeploymentCompleteSpec

---
+++ octopus-build-trigger-server/src/test/java/com/mjrichardson/teamCity/buildTriggers/DeploymentComplete/DeploymentCompleteSpecTest.java
@@ -0,0 +1,26 @@
+package com.mjrichardson.teamCity.buildTriggers.DeploymentComplete;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@Test
+public class DeploymentCompleteSpecTest {
+
+    @Test
+    public void two_param_ctor_returns_error_message() throws Exception {
+        DeploymentCompleteSpec sut = new DeploymentCompleteSpec("theurl", "theproject");
+        Assert.assertEquals(sut.getRequestorString(), "Unsuccessful attempt to get deployments for theproject on theurl");
+    }
+
+    @Test
+    public void when_successful_returns_success_message() throws Exception {
+        DeploymentCompleteSpec sut = new DeploymentCompleteSpec("theurl", "theproject", "theenv", true);
+        Assert.assertEquals(sut.getRequestorString(), "Successful deployment of theproject to theenv on theurl");
+    }
+
+    @Test
+    public void when_unsuccessful_returns_success_message() throws Exception {
+        DeploymentCompleteSpec sut = new DeploymentCompleteSpec("theurl", "theproject", "theenv", false);
+        Assert.assertEquals(sut.getRequestorString(), "Deployment of theproject on theurl");
+    }
+}
