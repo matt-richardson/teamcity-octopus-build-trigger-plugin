fba940474617c8a19d1404761901c6c1c31df8ad
Matt Richardson <matt@redgumtech.com.au>
2016-02-27 22:07:00 +0000

    Adding a few more tests for OctopusDate

--- octopus-build-trigger-server/src/test/java/com/mjrichardson/teamCity/buildTriggers/OctopusDateTest.java
+++ octopus-build-trigger-server/src/test/java/com/mjrichardson/teamCity/buildTriggers/OctopusDateTest.java
@@ -30,7 +30,12 @@ public class OctopusDateTest {
         Assert.assertEquals(sut.toString(), "2016-12-15T13:10:11.256+00:00");
     }
 
-    public void creating_date_with_day_month_year_hour_minute_second_sets_timezone_to_midnight() {
+    public void creating_date_with_day_month_year_sets_time_to_midnight_and_timezone_to_utc() {
+        OctopusDate sut = new OctopusDate(2016, 12, 15);
+        Assert.assertEquals(sut.toString(), "2016-12-15T00:00:00.000+00:00");
+    }
+
+    public void creating_date_with_day_month_year_hour_minute_second_sets_milliseconds_to_zero_and_timezone_to_utc() {
         OctopusDate sut = new OctopusDate(2016, 12, 15, 7, 15, 35);
         Assert.assertEquals(sut.toString(), "2016-12-15T07:15:35.000+00:00");
     }
@@ -56,5 +61,29 @@ public class OctopusDateTest {
         OctopusDate dateA = new OctopusDate(2016, 12, 15, 7, 15, 35, 123);
         OctopusDate dateB = new OctopusDate(2017, 12, 15, 7, 15, 35, 123);
         Assert.assertEquals(dateA.compareTo(dateB), -1);
+    }
+
+    public void equals_returns_true_for_equal_dates() {
+        OctopusDate dateA = new OctopusDate(2016, 12, 15, 7, 15, 35, 123);
+        OctopusDate dateB = new OctopusDate(2016, 12, 15, 7, 15, 35, 123);
+        Assert.assertTrue(dateA.equals(dateB));
+    }
+
+    public void equals_returns_false_when_passed_an_older_date() {
+        OctopusDate dateA = new OctopusDate(2016, 12, 15, 7, 15, 35, 123);
+        OctopusDate dateB = new OctopusDate(2015, 12, 15, 7, 15, 35, 123);
+        Assert.assertFalse(dateA.equals(dateB));
+    }
+
+    public void equals_returns_false_when_passed_an_newer_date() {
+        OctopusDate dateA = new OctopusDate(2016, 12, 15, 7, 15, 35, 123);
+        OctopusDate dateB = new OctopusDate(2017, 12, 15, 7, 15, 35, 123);
+        Assert.assertFalse(dateA.equals(dateB));
+    }
+
+    public void equals_returns_false_when_passed_a_different_object() {
+        OctopusDate dateA = new OctopusDate(2016, 12, 15, 7, 15, 35, 123);
+        String notADate = "a random string";
+        Assert.assertFalse(dateA.equals(notADate));
     }
 }
