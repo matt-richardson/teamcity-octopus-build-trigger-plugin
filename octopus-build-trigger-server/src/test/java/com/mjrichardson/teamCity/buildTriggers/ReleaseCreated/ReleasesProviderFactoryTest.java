123c18d265f14009f9d5df204fc27cc67d396b68
Matt Richardson <matt@redgumtech.com.au>
2016-03-04 14:21:19 +0000

    Adding tests for ReleasesProviderFactory

---
+++ octopus-build-trigger-server/src/test/java/com/mjrichardson/teamCity/buildTriggers/ReleaseCreated/ReleasesProviderFactoryTest.java
@@ -0,0 +1,25 @@
+package com.mjrichardson.teamCity.buildTriggers.ReleaseCreated;
+
+
+import com.mjrichardson.teamCity.buildTriggers.DeploymentComplete.DeploymentsProvider;
+import com.mjrichardson.teamCity.buildTriggers.DeploymentComplete.DeploymentsProviderFactory;
+import com.mjrichardson.teamCity.buildTriggers.DeploymentComplete.DeploymentsProviderImpl;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+
+@Test
+public class ReleasesProviderFactoryTest {
+    public void get_provider_returns_releases_provider() throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+        ReleasesProviderFactory sut = new ReleasesProviderFactory();
+        String url = "the-url";
+        String apiKey = "the-api-key";
+        Integer connectionTimeout = 100;
+        ReleasesProvider result = sut.getProvider(url, apiKey, connectionTimeout);
+
+        Assert.assertEquals(result.getClass(), ReleasesProviderImpl.class);
+    }
+}
