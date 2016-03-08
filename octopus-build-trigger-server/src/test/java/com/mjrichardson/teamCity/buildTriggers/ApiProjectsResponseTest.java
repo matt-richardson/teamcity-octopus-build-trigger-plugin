31000e718f750e11f3977985e5fc3e8125175ec6
Matt Richardson <matt@redgumtech.com.au>
2016-03-08 20:16:15 +0000

    Adding tests for ApiProjectsResponse

---
+++ octopus-build-trigger-server/src/test/java/com/mjrichardson/teamCity/buildTriggers/ApiProjectsResponseTest.java
@@ -0,0 +1,42 @@
+package com.mjrichardson.teamCity.buildTriggers;
+
+import org.json.simple.parser.ParseException;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+@Test
+public class ApiProjectsResponseTest {
+    public void can_parse_projects_response() throws IOException, ParseException {
+        final String json = ResourceHandler.getResource("api/projects");
+        ApiProjectsResponse sut = new ApiProjectsResponse(json);
+
+        Assert.assertFalse(sut.projects.isEmpty());
+        Assert.assertEquals(sut.projects.size(), 8);
+        Project[] array = sut.projects.toArray();
+        Assert.assertEquals(array[0].id, "Projects-24");
+        Assert.assertEquals(array[0].progressionApiLink, "/api/progression/Projects-24");
+        Assert.assertEquals(array[0].releasesApiLink, "/api/projects/Projects-24/releases");
+    }
+
+    public void can_parse_projects_response_with_no_projects() throws IOException, ParseException {
+        final String json = ResourceHandler.getResource("api/projects-with-no-items");
+        ApiProjectsResponse sut = new ApiProjectsResponse(json);
+
+        Assert.assertEquals(sut.projects.size(), 0);
+        Assert.assertTrue(sut.projects.isEmpty());
+    }
+
+    public void can_parse_projects_response_with_more_than_one_page() throws IOException, ParseException {
+        final String json = ResourceHandler.getResource("api/projects-with-more-than-one-page");
+        ApiProjectsResponse sut = new ApiProjectsResponse(json);
+
+        Assert.assertEquals(sut.projects.size(), 5);
+        Project[] array = sut.projects.toArray();
+        Assert.assertEquals(array[0].id, "Projects-24");
+        Assert.assertEquals(array[0].progressionApiLink, "/api/progression/Projects-24");
+        Assert.assertEquals(array[0].releasesApiLink, "/api/projects/Projects-24/releases");
+        Assert.assertEquals(sut.nextLink, "/api/projects?skip=5");
+    }
+}
