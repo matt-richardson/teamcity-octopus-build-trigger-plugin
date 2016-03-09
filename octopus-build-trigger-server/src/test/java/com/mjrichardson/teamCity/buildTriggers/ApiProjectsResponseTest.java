4c5d979ecf1274742950629ef344153902a7ee36
Matt Richardson <matt@redgumtech.com.au>
2016-03-09 22:05:26 +0000

    Update tests to have projects with more than one page of data

--- octopus-build-trigger-server/src/test/java/com/mjrichardson/teamCity/buildTriggers/ApiProjectsResponseTest.java
+++ octopus-build-trigger-server/src/test/java/com/mjrichardson/teamCity/buildTriggers/ApiProjectsResponseTest.java
@@ -13,11 +13,12 @@ public class ApiProjectsResponseTest {
         ApiProjectsResponse sut = new ApiProjectsResponse(json);
 
         Assert.assertFalse(sut.projects.isEmpty());
-        Assert.assertEquals(sut.projects.size(), 9);
+        Assert.assertEquals(sut.projects.size(), 30);
         Project[] array = sut.projects.toArray();
-        Assert.assertEquals(array[0].id, "Projects-24");
+        Assert.assertEquals(array[0].id, "Projects-143");
         Assert.assertEquals(array[0].progressionApiLink, "/api/progression/Projects-24");
         Assert.assertEquals(array[0].releasesApiLink, "/api/projects/Projects-24/releases");
+        Assert.assertEquals(sut.nextLink, "/api/projects?skip=30");
     }
 
     public void can_parse_projects_response_with_no_projects() throws IOException, ParseException {
@@ -26,17 +27,5 @@ public class ApiProjectsResponseTest {
 
         Assert.assertEquals(sut.projects.size(), 0);
         Assert.assertTrue(sut.projects.isEmpty());
-    }
-
-    public void can_parse_projects_response_with_more_than_one_page() throws IOException, ParseException {
-        final String json = ResourceHandler.getResource("api/projects-with-more-than-one-page");
-        ApiProjectsResponse sut = new ApiProjectsResponse(json);
-
-        Assert.assertEquals(sut.projects.size(), 5);
-        Project[] array = sut.projects.toArray();
-        Assert.assertEquals(array[0].id, "Projects-24");
-        Assert.assertEquals(array[0].progressionApiLink, "/api/progression/Projects-24");
-        Assert.assertEquals(array[0].releasesApiLink, "/api/projects/Projects-24/releases");
-        Assert.assertEquals(sut.nextLink, "/api/projects?skip=5");
     }
 }
