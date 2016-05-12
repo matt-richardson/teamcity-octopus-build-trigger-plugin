87dc005a671e1e026486ff60e5c988099e3772b6
Matt Richardson <matt@redgumtech.com.au>
2016-05-12 05:09:56 +0100

    Remove unused parameters

--- octopus-build-trigger-server/src/test/java/com/mjrichardson/teamCity/buildTriggers/DeploymentsTest.java
+++ octopus-build-trigger-server/src/test/java/com/mjrichardson/teamCity/buildTriggers/DeploymentsTest.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 
 @Test
 public class DeploymentsTest {
-    private Deployment getOctopusDeployment(String id, String environmentId, OctopusDate created, String serverTaskLink) {
+    private Deployment getOctopusDeployment(String id) {
         HashMap<String, Object> linksMap = new HashMap<>();
         linksMap.put("Task", "/api/tasks/ServerTasks-770");
         linksMap.put("Release", "/api/releases/Release-18");
@@ -26,36 +26,36 @@ public class DeploymentsTest {
 
     public void contains_returns_true_when_octopus_deployment_with_specified_id_exists() throws Exception {
         Deployments deployments = new Deployments();
-        deployments.add(getOctopusDeployment("Deployments-1", "Environments-1", new OctopusDate(2016, 3, 8), "/api/ServerTasks/Task-771"));
-        deployments.add(getOctopusDeployment("Deployments-2", "Environments-1", new OctopusDate(2016, 3, 9), "/api/ServerTasks/Task-772"));
+        deployments.add(getOctopusDeployment("Deployments-1"));
+        deployments.add(getOctopusDeployment("Deployments-2"));
         Assert.assertTrue(deployments.contains("Deployments-2"));
         Assert.assertEquals(deployments.size(), 2);
     }
 
     public void contains_returns_false_when_octopus_deployment_with_specified_id_does_not_exist() throws Exception {
         Deployments deployments = new Deployments();
-        deployments.add(getOctopusDeployment("Deployments-1", "Environments-1", new OctopusDate(2016, 3, 8), "/api/ServerTasks/Task-771"));
-        deployments.add(getOctopusDeployment("Deployments-2", "Environments-1", new OctopusDate(2016, 3, 9), "/api/ServerTasks/Task-772"));
+        deployments.add(getOctopusDeployment("Deployments-1"));
+        deployments.add(getOctopusDeployment("Deployments-2"));
         Assert.assertFalse(deployments.contains("Deployments-3"));
     }
 
     public void can_iterate_through_results() {
         Deployments deployments = new Deployments();
-        deployments.add(getOctopusDeployment("Deployments-1", "Environments-1", new OctopusDate(2016, 3, 8), "/api/ServerTasks/Task-771"));
-        deployments.add(getOctopusDeployment("Deployments-2", "Environments-1", new OctopusDate(2016, 3, 9), "/api/ServerTasks/Task-772"));
+        deployments.add(getOctopusDeployment("Deployments-1"));
+        deployments.add(getOctopusDeployment("Deployments-2"));
 
         Assert.assertEquals(deployments.size(), 2);
 
         Integer counter = 1;
-        for (Deployment octopusdeployment : deployments) {
-            Assert.assertEquals(octopusdeployment.deploymentId, "Deployments-" + counter++);
+        for (Deployment octopusDeployment : deployments) {
+            Assert.assertEquals(octopusDeployment.deploymentId, "Deployments-" + counter++);
         }
     }
 
     public void to_array_returns_all_items_as_array() {
         Deployments deployments = new Deployments();
-        deployments.add(getOctopusDeployment("Deployments-1", "Environments-1", new OctopusDate(2016, 3, 8), "/api/ServerTasks/Task-771"));
-        deployments.add(getOctopusDeployment("Deployments-2", "Environments-1", new OctopusDate(2016, 3, 9), "/api/ServerTasks/Task-772"));
+        deployments.add(getOctopusDeployment("Deployments-1"));
+        deployments.add(getOctopusDeployment("Deployments-2"));
 
         Deployment[] array = deployments.toArray();
         Assert.assertEquals(array.length, 2);
