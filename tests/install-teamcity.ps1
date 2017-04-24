5b2199b16819d15a505d3316c81732699d9410fa
Matt Richardson <matt@redgumtech.com.au>
2017-04-25 07:50:14 +1000

    Extract teamcity config repo to correct location

--- tests/install-teamcity.ps1
+++ tests/install-teamcity.ps1
@@ -5,4 +5,8 @@ write-host "Installing teamcity server"
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
 write-host "Expanding teamcity configuration git repository"
-Expand-Archive c:\TeamCityConfiguration.zip -DestinationPath c:\TeamCityConfiguration
+Expand-Archive c:\TeamCityConfiguration.zip -DestinationPath c:\
+
+#remove rubbish that mac has left behind
+Get-ChildItem 'C:\TeamCityConfiguration' -filter '.DS_Store' -Recurse | Remove-item
+Remove-Item 'C:\__MACOSX' -Recurse -Force
